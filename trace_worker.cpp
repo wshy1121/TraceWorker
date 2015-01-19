0d116c92e4e7688437456d3b27ef4aa104b6a23a
wshy1121 <wshy1121@qq.com>
2015-01-19 14:09:32 +0800

    优化长度转换接口

--- trace_worker.cpp
+++ trace_worker.cpp
@@ -362,6 +362,7 @@ void CLogDataInf::I2CLen(int iLen, char *CLen, int CLenSize)
 		iLen = iLen >> 8;
 		if (iLen == 0)
 		{
+			CLen[CLenSize-1] = i + 1;
 			break;
 		}
 	}
@@ -372,7 +373,7 @@ void CLogDataInf::C2ILen(char *CLen, int CLenSize, int &iLen)
 {
 	iLen = 0;
 	int x256 = 1;
-	for (int i=0; i<CLenSize; ++i)
+	for (int i=0; i<CLen[CLenSize-1]; ++i)
 	{
 		iLen += (unsigned char)(CLen[i])*x256;
 		x256 = x256 << 8;
