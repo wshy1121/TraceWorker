e82b3116f2b14fc81485242c08d060c2c23dae7b
huangyuan <wshy1121@qq.com>
2016-04-29 16:36:04 +0800

    [add]添加search接口 by huangyuan

--- rb_tree.h
+++ rb_tree.h
@@ -34,7 +34,7 @@ public:
         t->ch[0] = t->ch[1] = nil;  
         t->color = RED;  
         Node *x=head,*y=nil;  
-        while(x!=nil)  //寻找叶子作为插入的地方  
+        while(x!=nil)
         {  
             y = x;  
             x = x->ch[key > x->key];  
@@ -47,30 +47,34 @@ public:
     void remove(T &key)
     {  
         Node *t= head,*y;  
-        while(t!=nil&&t->key!=key) t = t->ch[key>t->key]; //查找被删除的结点  
+        while(t!=nil&&t->key!=key) t = t->ch[key>t->key];
         if(t==nil) return ;  
-        if(t->ch[0]==nil||t->ch[1]==nil) y = t;//y是真正要删除的结点  
+        if(t->ch[0]==nil||t->ch[1]==nil) y = t;
         else y = successor(t);  
         Node *x = y->ch[y->ch[0]==nil];  
         x->p = y->p;  
-        if(y->p==nil)//y是根  
+        if(y->p==nil)
         head = x;  
         else  
-        y->p->ch[y->p->ch[1]==y] = x; //把y从树中移除  
+        y->p->ch[y->p->ch[1]==y] = x;
         if(y!=t)  
         t->key = y->key;  
-        if(y->color==BLACK)//y是黑色则要进行红黑树的维护  
+        if(y->color==BLACK)
         remove_fixup(x);  
         delete(y);  
     }
-    
+
+    T *search(T &key)
+    {
+        return search(head, key);
+    }    
     void out()
     {  
         dfs(head,0);
         std::cout<<std::endl;  
     }  
 private:
-    Node *successor(Node *t)//返回t的后继结点  
+    Node *successor(Node *t)
     {
         Node *p;  
         if(t->ch[1]!=nil)
@@ -86,9 +90,9 @@ private:
             return p;  
         }  
     }
-    void rotate(Node *t,int c)///c=0 左旋 1 右旋  
+    void rotate(Node *t,int c)
     {  
-        Node *&pt = (t->p==nil) ? head : t->p->ch[t->p->ch[1]==t];// t的父亲指向t的指针  
+        Node *&pt = (t->p==nil) ? head : t->p->ch[t->p->ch[1]==t];
         Node *y = t->ch[c^1];  
         t->ch[c^1] = y->ch[c];  
         y->ch[c]->p = t;  
@@ -102,12 +106,12 @@ private:
     {  
         while(t->p->color==RED)
         {  
-            Node *p = t->p;//父亲R  
-            Node *g = p->p;//祖父B  
-            int pa = g->ch[1]==p;//父亲是左右孩子  
-            int ta = p->ch[1]==t;//自己是左右孩子  
-            Node *u = g->ch[pa^1];//叔叔  
-            if(u->color==RED)////case1：叔叔也是红色，改变p,u,t的颜色  
+            Node *p = t->p;
+            Node *g = p->p;
+            int pa = g->ch[1]==p;
+            int ta = p->ch[1]==t;
+            Node *u = g->ch[pa^1];
+            if(u->color==RED)
             {
                 g->color = RED;  
                 p->color = u->color = BLACK;  
@@ -115,15 +119,15 @@ private:
             }
             else
             {  
-                if(ta!=pa)//case2：不在一条直线上  
+                if(ta!=pa)
                 {  
-                    rotate(p,ta^1);//转成在同一条直线上  
+                    rotate(p,ta^1);
                     std::swap(t,p);  
                     ta = pa;  
                 }  
-                g->color = RED;//case3：改变p，g的颜色  
+                g->color = RED;
                 p->color = BLACK;  
-                rotate(g,pa^1);//把祖父旋转  
+                rotate(g,pa^1);
             }  
         }  
         head->color = BLACK;  
@@ -135,29 +139,28 @@ private:
         {  
             Node *p = t->p;  
             int ta = p->ch[1]==t;  
-            Node *w = p->ch[ta^1];//兄弟  
-            if(w->color==RED)//case1：兄弟是红色，兄弟染成黑色，父亲染成红色，  
+            Node *w = p->ch[ta^1];
+            if(w->color==RED)
             {  
                 w->color = BLACK;  
                 p->color = RED;  
                 rotate(p,ta);  
                 w = p->ch[ta^1];  
             }  
-            if(w->ch[0]->color==BLACK&&w->ch[1]->color==BLACK)//case2：两个侄子是黑色：把兄弟染红，把缺一个黑色结点的状态推给父亲  
+            if(w->ch[0]->color==BLACK&&w->ch[1]->color==BLACK)
             {
                 w->color = RED;  
                 t = p;  
             }
             else
             {  
-                if(w->ch[ta^1]->color == BLACK)//case3：有一个比较远的侄子是黑色更近的侄子是红色：把更近是侄子染黑，兄弟染红，兄弟旋转；  
+                if(w->ch[ta^1]->color == BLACK)
                 {
-                    w->ch[ta]->color = BLACK;    //转完之后的状态是，兄弟黑，更远的侄子红。  
+                    w->ch[ta]->color = BLACK;
                     w->color = RED;  
                     rotate(w,ta^1);  
                     w = p->ch[ta^1];  
                 }  
-                //case4：兄弟是黑色，较远的侄子是红色：兄弟染成父亲的颜色，父亲染黑，较远的侄子染黑，旋转父亲结点。  
                 w->color = p->color;  
                 p->color = BLACK;  
                 w->ch[ta^1]->color = BLACK;  
@@ -166,6 +169,28 @@ private:
             }  
         }  
         t->color = BLACK;  
+    }
+    
+    T *search(Node *head, T &key)   
+    {  
+        Node *t= head;
+        if (t ==NULL)   
+        {  
+            return NULL;  
+        }  
+
+        if (t->key == key)   
+        {  
+            return &t->key;  
+        }  
+        else if (key > t->key)   
+        {  
+            return search(t->ch[1], key);  
+        }   
+        else   
+        {  
+            return search(t->ch[0], key);  
+        }
     }
     void dfs(Node *t,int d)
     {  
@@ -178,7 +203,7 @@ private:
         dfs(t->ch[1],d+(t->color==BLACK));  
     }
 private:    
-    Node *head,*nil;//根结点，叶子结点  
+    Node *head,*nil;
 };  
 
 #endif
